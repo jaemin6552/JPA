a8f3613e1e1ba1041e76782dc482860c983f2fe4
KJM <jaemin6552@Naver.com>
2023-07-11 04:16:49 +0900

    JPA Study

--- JPA/src/main/java/home/JPA/service/impl/QuizService.java
+++ JPA/src/main/java/home/JPA/service/impl/QuizService.java
@@ -4,14 +4,17 @@ import home.JPA.dto.QuizDto;
 import home.JPA.entity.memberQuiz.MemberQuizEntity;
 import home.JPA.entity.quiz.QuizEntity;
 import home.JPA.repository.MemberQuizRepository;
+import home.JPA.repository.MemberRepository;
 import home.JPA.repository.QuizRepository;
 import lombok.AllArgsConstructor;
+import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
 public class QuizService {
     private QuizRepository quizRepository;
     private MemberQuizRepository memberQuizRepository;
+    private MemberRepository memberRepository;
 
     public List<QuizDto> getAllQuizzes(String language){
         Pageable pageable = PageRequest.of(0, 5);
@@ -29,7 +33,15 @@ public class QuizService {
         }
         return quizDtoList;
     }
-
+    public boolean updateQuizMember(String email,long quizId){
+        MemberQuizEntity memberQuizEntity = new MemberQuizEntity();
+        memberQuizEntity.setTry(true);
+        memberQuizEntity.setRight(true);
+        memberQuizEntity.setMember(memberRepository.findByEmail(email).orElseThrow(()-> new NoSuchElementException("유저없음")));
+        memberQuizEntity.setQuizEntity(quizRepository.getReferenceById(quizId));
+        memberQuizRepository.save(memberQuizEntity);
+        return true;
+    }
     public List<QuizDto> getUnansweredQuizzes(String email,String language) {
         List<MemberQuizEntity> correctMemberQuizzes = memberQuizRepository.findCorrectQuizzesForMemberId(email);
         List<QuizEntity> correctQuizzes = correctMemberQuizzes.stream()
